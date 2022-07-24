d3913784bdf388a98dc7cb0bdede2146b27674df
Sergio Peña <sergiop79@gmail.com>
2022-07-24 11:04:38 +0200

    Chapter 370

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,21 @@ const {
   loginUsuario,
   revalidarToken,
 } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
 // Crear nuevo usuario
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "La contraseña es obligatoria").isLength({ min: 6 }),
+    validarCampos,
+  ],
+  crearUsuario
+);
 
 // Login de usuario
 router.post(
@@ -17,6 +27,7 @@ router.post(
   [
     check("email", "El email es obligatorio").isEmail(),
     check("password", "La contraseña es obligatoria").isLength({ min: 6 }),
+    validarCampos,
   ],
   loginUsuario
 );
