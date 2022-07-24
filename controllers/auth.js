f221450bfcd3ca8822cbd3290590861e83e4bb79
Sergio Peña <sergiop79@gmail.com>
2022-07-24 17:47:02 +0200

    Chapter 375

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,45 @@
 const { response } = require("express");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
+  const { email, name, password } = req.body;
+
+  try {
+    // Verificar email
+    const usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario ya existe con ese email",
+      });
+    }
+
+    // Crear usuario con el modelo
+
+    const dbUser = new Usuario(req.body);
+
+    // Hashear la contraseña
+
+    // Generar el JWT
+
+    // Crear usuario de base de datos
+    await dbUser.save();
+
+    // Generar respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      uid: dbUser.id,
+      name,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
+
   return res.json({
     ok: true,
     msg: "Crear usuario /new",
